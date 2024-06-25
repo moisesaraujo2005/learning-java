e3bba2a6daba6a4c4f21a6c9eedc821f1caec224
moisesaraujo2005 <moises.silva@sptech.school>
2024-06-25 11:27:21 -0300

    Java

---
+++ aprendendo/variaveisTentativa.java
@@ -0,0 +1,20 @@
+public class variaveisTentativa {
+
+public static void main(String[] args) {
+    String nome = "Moisés";
+
+    int a;
+    int b = 5;
+    a = 9;
+
+    int média =  (a+b)/2;
+    int soma = a+b;
+    int subtração= a-b;
+    int multiplicação = a*b;
+    float divisão = (float) a/b;
+    System.out.println("Olá, " + nome  + " Sua média é " + média  +
+            " sua soma é  " + soma + " sua subtração é  " + subtração  +
+            " sua multiplicação é " + multiplicação  +
+            " sua divisão é  " + divisão
+    );
+} }
